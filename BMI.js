f936a29bd40fd5d3fca60c1edfbefa31ebbc3764
Led <sunmoondevil@gmail.com>
2015-10-27 13:46:18 +0800

    練習-2

--- BMI.js
+++ BMI.js
@@ -1,6 +1,6 @@
 var BMI = function(){
-	var 身高 = prompt("請輸入身高(公分)")
-    var 體重 = prompt("請輸入體重(公斤)")
-    console.log(體重 / Math.pow(身高/100 , 2))
+	var Height = prompt("請輸入身高(公分)")
+    var Weight = prompt("請輸入體重(公斤)")
+    console.log(Weight / Math.pow(Height/100 , 2))
 }
 BMI()
